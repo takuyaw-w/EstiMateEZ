2eac80de3b91ad07ac2f8f7ca5fbc349b18209a0
takuyaw-w <takuya.jamtb@gmail.com>
2023-10-11 22:51:31 +0900

    fix: vuetify導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  modules: [
+    './modules/vuetify.ts'
+  ],
   devtools: { enabled: true }
 })
