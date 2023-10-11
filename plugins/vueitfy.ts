2e46ccfdc9268bc916dfaa01369a8de397b0f526
takuyaw-w <takuya.jamtb@gmail.com>
2023-10-11 23:06:14 +0900

    fix: font icon追加

--- plugins/vueitfy.ts
+++ plugins/vueitfy.ts
@@ -1,8 +1,18 @@
 import {createVuetify} from 'vuetify'
+import {aliases, mdi} from 'vuetify/iconsets/mdi-svg'
 
 export default defineNuxtPlugin(nuxtApp => {
     const vuetify = createVuetify({
-        ssr: false
+        ssr: false,
+        icons: {
+            defaultSet: 'mdi',
+            aliases: {
+                ...aliases,
+            },
+            sets: {
+                mdi
+            }
+        }
     })
     nuxtApp.vueApp.use(vuetify)
 })
